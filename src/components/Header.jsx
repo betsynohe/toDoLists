a48fc8f71e76795658c45a41076564a5c503ba0a
betsynohe <betsyguitian49@gmail.com>
2023-12-05 21:43:16 -0300

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Heading, Container, Image} from '@chakra-ui/react'
+import Imagen1 from '../assets/img/imagen1.png'
+
+
+import React from 'react'
+
+export default function Header() {
+    return (
+        <Container display="flex" alignItems="center" justifyContent="center" mt="50px">
+            <Image boxSize='100px' src={Imagen1} alt='task' />
+            <Heading size='xl' ml="20px">Mis Tareas</Heading>
+        </Container>
+    )
+}
